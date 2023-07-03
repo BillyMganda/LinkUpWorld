60c4f6e266bef8990eff8e4269545e2666b0b7fb
Billy <bimganda@gmail.com>
2023-07-03 16:14:46 -0400

    create user class

---
+++ LinkUpWorld.UsersMicroservice/Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkUpWorld.UsersMicroservice.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Handle { get; set; } = string.Empty;
+        public string PasswordSalt { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;        
+        public string Bio { get; set; } = string.Empty;
+        public string ProfilePicture { get; set; } = string.Empty;
+        public DateTime JoinDate { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}
