68344113854a8af242e0f537913abec4bb387973
Billy <bimganda@gmail.com>
2023-07-03 16:34:43 -0400

    install database related nuget packages

--- LinkUpWorld.UsersMicroservice/Infrastructure/Repositories/UserRepository.cs
+++ LinkUpWorld.UsersMicroservice/Infrastructure/Repositories/UserRepository.cs
@@ -1,8 +1,14 @@
 ﻿using LinkUpWorld.UsersMicroservice.Domain.Repositories;
+using LinkUpWorld.UsersMicroservice.Infrastructure.Data;
 
 namespace LinkUpWorld.UsersMicroservice.Infrastructure.Repositories
 {
     public class UserRepository : IUserRepository
     {
+        private readonly UserDbContext _userDbContext;
+        public UserRepository(UserDbContext userDbContext)
+        {
+            _userDbContext = userDbContext;
+        }
     }
 }
