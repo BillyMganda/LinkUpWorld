f66688ac0031829941e2206d2f58f607f97cb2ce
Billy <bimganda@gmail.com>
2023-07-03 17:46:05 -0400

    create users controller and add method to create a new user

---
+++ LinkUpWorld.UsersMicroservice/Controllers/usersController.cs
@@ -0,0 +1,26 @@
+﻿using LinkUpWorld.UsersMicroservice.Application.CQRS.Users.Commands;
+using LinkUpWorld.UsersMicroservice.Application.CQRS.Users.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkUpWorld.UsersMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public usersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GetUserDto>> CreateCategory([FromBody] CreateUserCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            return Created("", result);
+        }
+    }
+}
