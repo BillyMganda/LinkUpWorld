90ae03016afe16656dce6820de94aabf31b9e172
Billy <bimganda@gmail.com>
2023-07-04 06:20:12 -0400

    add query class to search a user by their name

---
+++ LinkUpWorld.UsersMicroservice/Application/CQRS/Users/Queries/QueryUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using LinkUpWorld.UsersMicroservice.Application.CQRS.Users.DTOs;
+using MediatR;
+
+namespace LinkUpWorld.UsersMicroservice.Application.CQRS.Users.Queries
+{
+    public class QueryUserQuery : IRequest<IEnumerable<GetUserDto>>
+    {
+        public string Name { get; set; }
+        public QueryUserQuery(string name)
+        {
+            Name = name;
+        }
+    }
+}
