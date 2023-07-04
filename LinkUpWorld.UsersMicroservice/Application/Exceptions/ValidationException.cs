84a7f9e876b82d80810e19afc2123cca10d669fa
Billy <bimganda@gmail.com>
2023-07-04 07:10:53 -0400

    add ValidationException exception

---
+++ LinkUpWorld.UsersMicroservice/Application/Exceptions/ValidationException.cs
@@ -0,0 +1,11 @@
+﻿namespace LinkUpWorld.UsersMicroservice.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public IEnumerable<string> Errors { get; }
+        public ValidationException(IEnumerable<string> errors) : base("One or more validation errors occurred.")
+        {
+            Errors = errors;
+        }
+    }
+}
