8eec636219fe39bd77daaa70fa6412c58e8b85d5
Billy <bimganda@gmail.com>
2023-07-04 05:55:37 -0400

    create command class and its handler class for deleting user data

---
+++ LinkUpWorld.UsersMicroservice/Application/CQRS/Users/Commands/DeleteUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace LinkUpWorld.UsersMicroservice.Application.CQRS.Users.Commands
+{
+    public class DeleteUserCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ LinkUpWorld.UsersMicroservice/Application/CQRS/Users/Handlers/DeleteUserCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using LinkUpWorld.UsersMicroservice.Application.CQRS.Users.Commands;
+using LinkUpWorld.UsersMicroservice.Domain.Repositories;
+using MediatR;
+
+namespace LinkUpWorld.UsersMicroservice.Application.CQRS.Users.Handlers
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, Unit>
+    {
+        private readonly IUserRepository _userRepository;
+        public DeleteUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.Id);
+
+            if(user != null)
+            {
+                await _userRepository.DeleteAsync(request.Id);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
